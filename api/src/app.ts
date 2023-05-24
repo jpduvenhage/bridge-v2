90d0f21a2362df56359ba6d963594983badaac17
Brian Ramirez <ramirez7358@gmail.com>
2023-05-24 15:03:01 -0300

    Add new endpoint

--- api/src/app.ts
+++ api/src/app.ts
@@ -15,6 +15,100 @@ createConnection().then(async (connection) => {
   const wsProvider = new WsProvider(process.env.WS_NODE);
   const api = await ApiPromise.create({ provider: wsProvider });
 
+  const getGlitchInfo = async (glitchHash: string) => {
+    let signedBlock: SignedBlock;
+    try {
+      console.log(
+        `[${new Date().toLocaleString()}] - Asking the node for block information: ${glitchHash}`
+      );
+      signedBlock = await api.rpc.chain.getBlock(glitchHash);
+    } catch (error) {
+      console.error(`[${new Date().toLocaleString()}] - ${error}`);
+    }
+
+    let netAmount: string;
+    let extrinsicHash: string;
+
+    signedBlock.block.extrinsics.forEach((ex, index) => {
+      // the extrinsics are decoded by the API, human-like view
+      //console.log(index, ex.toHuman());
+
+      const {
+        isSigned,
+        meta,
+        method: { args, method, section },
+      } = ex;
+
+      // explicit display of name, args & documentation
+      console.info(
+        `[${new Date().toLocaleString()}] - ${section}.${method}(${args
+          .map((a) => a.toString())
+          .join(", ")})`
+      );
+
+      const x = args.map((a) => a.toString());
+      netAmount = x.at(1);
+      extrinsicHash = ex.hash.toHex();
+
+      // signer/nonce info
+      if (isSigned) {
+        console.info(
+          `[${new Date().toLocaleString()}] - signer=${ex.signer.toString()}, nonce=${ex.nonce.toString()}`
+        );
+      }
+    });
+    return { netAmount, extrinsicHash };
+  };
+
+  app.get("/api/transactionHistory/:wallet", async (request, response) => {
+    console.info(
+      `[${new Date().toLocaleString()}] - Obtaining the transaction history of address ${
+        request.params.wallet
+      }`
+    );
+
+    const txs = await txRepository.find({
+      where: [
+        { from_eth_address: request.params.wallet },
+        { to_glitch_address: request.params.wallet },
+      ],
+    });
+
+    console.info(
+      `[${new Date().toLocaleString()}] - ${txs.length} transactions was found!`
+    );
+
+    const txsWithInfo = txs.map(async (tx) => {
+      if (tx.extrinsic_hash && tx.net_amount) {
+        console.info(
+          `[${new Date().toLocaleString()}] - Net amount and extrinsic hash of transaction ${
+            tx.id
+          } already exists in the database.`
+        );
+      } else {
+        getGlitchInfo(tx.tx_glitch_hash)
+          .then(async (glitchInfo) => {
+            tx.extrinsic_hash = glitchInfo.extrinsicHash;
+            tx.net_amount = glitchInfo.netAmount;
+            await txRepository.save(tx);
+          })
+          .catch((error) => {
+            console.error(
+              `[${new Date().toLocaleString()}] - Net amount and extrinsic hash of transaction ${
+                tx.id
+              } already exists in the database.`
+            );
+            console.error(error);
+            return tx;
+          });
+      }
+
+      return tx;
+    });
+
+    return response.json(await Promise.all(txsWithInfo));
+  });
+
   app.get("/api/transactionInfo/:eth_tx", async (request, response) => {
     console.info(
       `[${new Date().toLocaleString()}] - Getting information from eth transaction with id ${

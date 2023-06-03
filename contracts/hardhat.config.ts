da17b3ecfc13b3a81e4f52a9f08bc6545170a679
Brian Ramirez <ramirez7358@gmail.com>
2023-06-02 23:14:37 -0300

    Add mainet network in hardhat config

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,30 +1,37 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-const GOERLI_PRIVATE_KEY = "698b7c0f189872b77abf81107003ed2ec1ff7060090dac0bbc84a1b012ececef";
+const GOERLI_PRIVATE_KEY =
+  "698b7c0f189872b77abf81107003ed2ec1ff7060090dac0bbc84a1b012ececef";
 const ALCHEMY_API_KEY = "XsoDxjFTZiqbFPK_1-nn7meNNzGq-rR5";
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.17",
+    version: "0.8.18",
     settings: {
       optimizer: {
         enabled: true,
-        runs: 200
-      }
-    }
+        runs: 200,
+      },
+    },
   },
   networks: {
     goerli: {
       url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [GOERLI_PRIVATE_KEY],
     },
+    mainnet: {
+      url: `https://eth-mainnet.g.alchemy.com/v2/OjcHiAIkMzQxjmJelPHLV7mhhpHHI8Uq`,
+      accounts: [GOERLI_PRIVATE_KEY],
+    },
     bsctestnet: {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
       chainId: 97,
       gasPrice: 20000000000,
-      accounts: ["7269264f5b19119d3474a9e81ae0ff67f3ae8b396193e3e19be4a78873f0e971"]
-    }
+      accounts: [
+        "7269264f5b19119d3474a9e81ae0ff67f3ae8b396193e3e19be4a78873f0e971",
+      ],
+    },
   },
   etherscan: {
     apiKey: "CIANIPEHWQPCMY19IWUMQAXAC84Y3F9MU7",
